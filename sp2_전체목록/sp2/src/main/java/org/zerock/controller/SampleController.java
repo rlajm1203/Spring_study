9f0833b419adc93dcb2e1cefb852a3e83496c308
rlajm1203 <rlajm1203@naver.com>
2023-09-09 00:07:09 +0900

    2023.09.09 - Spring과 MySQL을 연동한 프로젝트 - Mac OS에서 사용할 때 참고 가능

---
+++ sp2_전체목록/sp2/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,119 @@
+package org.zerock.controller;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(
+//				java.util.Date.class, 
+//				new CustomDateEditor(df, false));
+//	}
+	
+//	@GetMapping("/ex4")
+//	public ModelAndView ex4(SampleDTO dto, @ModelAttribute("page") int page ) {
+//		
+//		log.info("ex4....................");
+//		log.info(dto);
+//		log.info(page);
+//		
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("/sample/ex4");
+//		mav.addObject("sampleDTO", dto);
+//		mav.addObject("result", "SUCCESS");
+//		return mav;
+//	}
+	
+	@GetMapping("/ex5")
+	public String ex5(RedirectAttributes rttr) {
+		log.info("ex5..................");
+		
+		rttr.addFlashAttribute("result", "SUCCESS");
+		
+		return "redirect:/sample/ex6";
+	}
+
+	@GetMapping("/ex6")
+	public void ex6() {
+		log.info("ex6..................");
+	}
+	
+	@GetMapping("/ex4")
+	public String ex4(SampleDTO dto, @ModelAttribute("page") int page, Model model) {
+		
+		log.info("ex4....................");
+		log.info(dto);
+		log.info(page);
+		
+		model.addAttribute("result", "SUCCESS");
+		
+		return "/sample/ex4";
+	}
+	
+	@GetMapping("/ex3")
+	public void ex3(TodoDTO todo) {
+		log.info(todo);
+	}
+	
+	@GetMapping("/ex3A")
+	public void ex3A(TodoDTO todo) {
+		log.info(todo);
+	}
+	
+
+	@GetMapping("/ex1")
+	public void doA(SampleDTO dto) {
+		
+		log.info(dto);
+	}
+	
+	@GetMapping("/ex2")
+	public void ex2(
+			@RequestParam("name") String name, 
+			@RequestParam("age") int age ) {
+		
+		log.info(name);
+		log.info(age);
+	}
+	
+	@GetMapping("/ex2Arr")
+	public void ex2(
+			@RequestParam("ids") String[] ids) {
+		
+		log.info(Arrays.toString(ids));
+	}	
+	
+	//@GetMapping("/ex2Bean")
+	@RequestMapping(value = "/ex2Bean" , 
+			method = {RequestMethod.GET, RequestMethod.POST } )
+	public void ex2Bean(SampleDTOList list) {
+		
+		log.info(list);
+	}	
+	
+	
+}
+
+
+
+
+
---
+++ sp2_전체목록/sp2/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+ <table border="1">
+   <tr>
+     <td>BNO</td>
+     <td>TITLE</td>
+     <td>WRITER</td>
+     <td>REGDATE</td>
+   </tr>
+   <c:forEach items="${list}" var="vo">
+   <tr>
+     <td><c:out value="${vo.bno}"/></td>
+     <td><c:out value="${vo.title}"/></td>
+     <td><c:out value="${vo.writer}"/></td>
+     <td><c:out value="${vo.regdate}"/></td>
+   </tr>
+   </c:forEach>
+ </table>
+</body>
+</html>
